ab03ee3ae499351460b06bd00559a1ad17006c6e
HARSAWASTHI <harsh123awasthi@gmail.com>
2026-03-25 19:13:20 +0530

    monitoring metrics

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 from flask import Flask
+from prometheus_client import Counter, generate_latest
 import os
 
 app = Flask(__name__)
 
+REQUEST_COUNT = Counter('request_count', 'Total HTTP Requests')
+
 @app.route("/")
 def home():
-    return "Hello DevOps v3 - Running in Production"
+    REQUEST_COUNT.inc()
+    return "Hello DevOps - Running in Production"
 
 @app.route("/health")
 def health():
     return {"status": "ok"}
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest, 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
